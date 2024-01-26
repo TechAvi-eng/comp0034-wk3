4d2f0cf49993deb2e25e0d7aa488443fd8638a64
TechAvi-eng <n.dhulashia@gmail.com>
2024-01-26 13:35:10 +0000

    Working on Week 3 Activites

--- paralympics/models.py
+++ paralympics/models.py
@@ -1,4 +1,6 @@
 # Adapted from https://flask-sqlalchemy.palletsprojects.com/en/3.1.x/quickstart/#define-models
+import json
+
 from typing import List
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
